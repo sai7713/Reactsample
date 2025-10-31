b469551aef1ec185a13c82447f35f6ccdd0ba326
sai <81062606+sai7713@users.noreply.github.com>
2025-10-31 08:20:32 +0530

    Add files via upload

---
+++ Form_Data/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SubmitForm from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SubmitForm />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
